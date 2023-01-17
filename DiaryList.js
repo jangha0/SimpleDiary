11bcc06abe291911ed13885c274e2a3794dbcd47
jangha0 <jangkate1104@gmail.com>
2023-01-18 01:46:23 +0900

    1

--- DiaryList.js
+++
@@ -1,9 +0,0 @@
-const DiaryList = () => {
-  return (
-    <div className="diaryList">
-      <h2>일기 리스트</h2>
-    </div>
-  );
-};
-
-export default DiaryList;
--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React, { useRef, useState } from "react";
-import DiaryList from "../DiaryList";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList.js";
 import "./App.css";
 
-function App() {
+const dummyList = [
+  {
+    id: 1,
+    author: "레니니",
+    content: "헤이 1",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "죠르디",
+    content: "헤이 2",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "개굴이",
+    content: "헤이 3",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "알린",
+    content: "헤이 4",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
+
+const App = () => {
   return (
     <div className="App">
       <DiaryEditor />
-      <DiaryList />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
-}
+};
 
 export default App;
